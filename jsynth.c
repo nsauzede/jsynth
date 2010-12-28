7afa51076de5f7f63bebec518c6f9cf92cf7e290
Nicolas Sauzede <nsauzede@laposte.net>
2010-12-28 02:05:38 +0100

    sample period/width/freq at start of process

--- jsynth.c
+++ jsynth.c
@@ -16,12 +16,13 @@ int square_not_tri = 1;
 
 //int period = 60 * sampleFrequency / tempo / steps;	// ms
 //int period = 251;	// ms
-int period = 0;	// ms
-//int width = ;	// ms
-int width = 132;	// ms
+int __period = 0;	// ms
+//int width = period / 2;	// ms
+//int __width = 132;	// ms
+int __width = 0;	// ms
 //int freq = 15.7 + note * 2.5 * 2 * octave;		// Hz
 //int freq = 440;		// Hz
-int freq = 0;		// Hz
+int __freq = 0;		// Hz
 
 int next_t = 0;		// sample
 int t = 0;			// sample
@@ -33,24 +34,27 @@ int process_audio( jack_nframes_t nframes, void *arg) {
 	jack_port_t *port = arg;
 	sample_t *stream = jack_port_get_buffer( port, nframes);
 	int i;
-	unsigned int bytesPerPeriod = sampleFrequency / freq;
+	int _freq = __freq;
+	int _period = __period;
+	int _width = __width;
+	unsigned int bytesPerPeriod = sampleFrequency / _freq;
 	for (i = 0; i < nframes; i++) {
 		sample_t s = 0;
 		if (t >= next_t) {
-			next_t = t + period * sampleFrequency / 1000;
+			next_t = t + _period * sampleFrequency / 1000;
 			pos = 0;
 		}
 		if (pos >= 0) {
-			if (pos >= (width * sampleFrequency / 1000)) {
-				next_t = t + (period - width) * sampleFrequency / 1000;
+			if (pos >= (_width * sampleFrequency / 1000)) {
+				next_t = t + (_period - _width) * sampleFrequency / 1000;
 				pos = -1;
 			}
 			else {
 				double amp = 0.5;
 				int decay = 10;						// %
-				int decay_dur = (width * sampleFrequency / 1000) * decay / 100;
-				int decay_start = (width * sampleFrequency / 1000) - decay_dur;
-				int decay_end = (width * sampleFrequency / 1000);
+				int decay_dur = (_width * sampleFrequency / 1000) * decay / 100;
+				int decay_start = (_width * sampleFrequency / 1000) - decay_dur;
+				int decay_end = (_width * sampleFrequency / 1000);
 				if ((pos >= decay_start) && (pos < decay_end))
 					amp *= ((double)decay_dur - (pos - decay_start)) / decay_dur;
 //				printf( "pos=%d width=%d decay=%d decay_dur=%d decay_start=%d decay_end=%d amp=%f\n", pos, width, decay, decay_dur, decay_start, decay_end, amp);
@@ -66,7 +70,7 @@ int process_audio( jack_nframes_t nframes, void *arg) {
 				}
 				s *= amp * volume / 100;
 				printf( "pos=%d bytesPerPeriod=%d width=%d decay=%d decay_dur=%d decay_start=%d decay_end=%d amp=%.1f s=%.1f\n",
-					pos, bytesPerPeriod, width, decay, decay_dur, decay_start, decay_end, amp, s);
+					pos, bytesPerPeriod, _width, decay, decay_dur, decay_start, decay_end, amp, s);
 				pos++;
 			}
 		}
@@ -103,9 +107,10 @@ int main( int argc, char *argv[]) {
 	} else {
 		printf( "jack server not running ?\n");
 	}
-	period = 1000 * 60 / tempo / steps;	// ms
-	freq = ((double)15.7 + 363.6 * tune / 100 + note * 2.5) * 2 * octave;		// Hz
-	printf( "period=%d freq=%d\n", period, freq);
+	__period = 1000 * 60 / tempo / steps;	// ms
+	__width = __period / 2;	// ms
+	__freq = ((double)15.7 + 363.6 * tune / 100 + note * 2.5) * 2 * octave;		// Hz
+	printf( "period=%d freq=%d\n", __period, __freq);
 	int done = 0;
 	while (!done) {
 		SDL_Event event;
